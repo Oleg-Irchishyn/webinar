126b3819a47e58997726133a2f91e9a9e3a9d759
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2021-03-17 18:01:52 +0200

    initializing gh-pages

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-// Entrypoint (For imports only)
-import 'bootstrap';
-import 'lazysizes';
-import './components/libs/slick-carousel/slick/slick.min';
-import './components/libs/OverlayScrollbars/jquery.overlayScrollbars';
-import './components/common';
